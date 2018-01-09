4b075635c159b89cb98c5bf1d5be9099ee44a5be
warlord25081 <34706554+warlord25081@users.noreply.github.com>
2018-01-09 12:25:17 +0100

    create PDF

---
+++ src/main/java/PDF_Test/CreatePdf.java
@@ -0,0 +1,14 @@
+package PDF_Test;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CreatePdf implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
