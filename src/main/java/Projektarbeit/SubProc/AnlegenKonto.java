23f5c78073d5d0271c1bb22d17c97ba1c2c73ac0
warlord25081 <34706554+warlord25081@users.noreply.github.com>
2018-01-09 13:20:55 +0100

    update

--- src/main/java/Projektarbeit/SubProc/AnlegenKonto.java
+++ src/main/java/Projektarbeit/SubProc/AnlegenKonto.java
@@ -1,6 +1,7 @@
 package Projektarbeit.SubProc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.zip.DataFormatException;
@@ -10,33 +11,35 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import CamundaProjekt.leihVorgangStuS.Datenbankzugang;
+
 public class AnlegenKonto implements JavaDelegate{
 
 	private static final Logger L = LoggerFactory.getLogger(AnlegenKonto.class);
-	
-	private Connection connection;
-		public void setConnection(Connection connection) {
-		this.connection = connection;
-	}//end of connection
-	
-	private Connection getConnection() {
-		if (connection == null) {
-			try {
-				throw new Exception("Connection not set");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return connection;
-	}//end of getConnection 
 
 	public void execute(DelegateExecution execution) throws Exception {
 	
+		Connection conn = null;
+		try {
+			L.info("* Treiber laden");
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		} catch (Exception e) {
+			L.error("Unable to load driver.");
+			e.printStackTrace();
+		}
+		try {
+			L.info("* Verbindung aufbauen");
+			String url = "jdbc:mysql://" + Datenbankzugang.hostname + ":" + Datenbankzugang.port + "/" + Datenbankzugang.dbname;
+			conn = DriverManager.getConnection(url, Datenbankzugang.user, Datenbankzugang.password);			
+		} catch (SQLException sqle) {
+			L.error("SQLException: " + sqle.getMessage() + "/n SQLState: " + sqle.getSQLState() + " VendorError: " + sqle.getErrorCode());
+
+		}
 		L.info("Start einlesen von Personendaten");
 		String sql = "insert into Person(anrede, vorname, nachname, matrikelnummer, adresse, plz, wohnort, eMailAdresse" + 
 				" values (?, ?, ?, ?, ?, ?, ?, ?)";
 		L.info(sql);
-		try(PreparedStatement s = connection.prepareStatement(sql)){
+		try(PreparedStatement s = conn.prepareStatement(sql)){
 			s.setString(1, (String) execution.getVariable("anrede"));
 			s.setString(2, (String) execution.getVariable("vorname"));
 			s.setString(3, (String) execution.getVariable("nachname"));
